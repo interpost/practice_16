9e7a061d2800478c05ccf88db942195555b1260e
interpost <74204798+interpost@users.noreply.github.com>
2020-11-23 19:46:07 +0300

    Add files via upload

---
+++ cat.py
@@ -0,0 +1,14 @@
+class cat:
+    def _init_(self, name, gender, age):
+        self.name = name
+        self.type = gender
+        self.age = age
+
+        def get_name(self):
+        	return self.name
+
+        def get_gender(self):
+            return self.gender
+
+        def get_age(self):
+            return self.age
---
+++ cat_import.py
@@ -0,0 +1,13 @@
+from Pets import Cat
+
+cat1 = Cat("Барон", "Мальчик", "2 года")
+
+print("Name of the cat1", cat1.getName())
+print("Gender of cat1", cat1.getName(), cat1.getGender())
+print("Age of cat1 = ", cat1.getName(), cat1.getAge())
+
+cat2 = Cat("Сэм", "Мальчик", "2 года")
+
+print("Name of the cat2", cat2.getName())
+print("Gender of cat2", cat2.getName(), cat2.getGender())
+print("Age of cat2 = ", cat2.getName(), cat2.getAge())
